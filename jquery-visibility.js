6a6a4400563738cb68a1ceaacde3346e99265910
xmikex83 <xmikex83@gmail.com>
2014-03-11 16:02:59 +0100

    Refactoring for browserify

--- jquery-visibility.js
+++ jquery-visibility.js
@@ -1,18 +1,15 @@
-/*! http://mths.be/visibility v1.0.7 by @mathias | MIT license */
-;(function(window, document, $, undefined) {
+module.exports = function($) {
 
 	var prefix;
 	var property;
 	// In Opera, `'onfocusin' in document == true`, hence the extra `hasFocus` check to detect IE-like behavior
-	var eventName = 'onfocusin' in document && 'hasFocus' in document
-		? 'focusin focusout'
-		: 'focus blur';
+	var eventName = 'onfocusin' in document && 'hasFocus' in document ? 'focusin focusout' : 'focus blur';
 	var prefixes = ['webkit', 'o', 'ms', 'moz', ''];
 	var $support = $.support;
 	var $event = $.event;
 
 	while ((prefix = prefixes.pop()) != undefined) {
-		property = (prefix ? prefix + 'H': 'h') + 'idden';
+		property = (prefix ? prefix + 'H' : 'h') + 'idden';
 		if ($support.pageVisibility = typeof document[property] == 'boolean') {
 			eventName = prefix + 'visibilitychange';
 			break;
@@ -35,21 +32,17 @@
 		// should both be `null` or `undefined`; else, the page visibility hasn’t
 		// changed, but the user just clicked somewhere in the doc. In IE9, we need
 		// to check the `relatedTarget` property instead.
-		if (
-			!/^focus./.test(type) || (
-				toElement == undefined &&
-				originalEvent.fromElement == undefined &&
-				originalEvent.relatedTarget == undefined
-			)
-		) {
+		if (!/^focus./.test(type) || (
+			toElement == undefined &&
+			originalEvent.fromElement == undefined &&
+			originalEvent.relatedTarget == undefined
+		)) {
 			$event.trigger(
 				(
-					property && document[property] || /^(?:blur|focusout)$/.test(type)
-						? 'hide'
-						: 'show'
+					property && document[property] || /^(?:blur|focusout)$/.test(type) ? 'hide' : 'show'
 				) + '.visibility'
 			);
 		}
 	});
 
-}(this, document, jQuery));
+};
